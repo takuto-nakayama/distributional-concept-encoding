c0d79e7a0887191bcba3863d280aa2d92077a1d5
Takuto Nakayama <tnakayama.a5ling@gmail.com>
2025-06-02 07:55:36 +0900

    20250602: add saving function and directory

--- classes.py
+++ classes.py
@@ -1,31 +1,75 @@
 from transformers import BertTokenizer, BertModel
 from scipy.stats import gaussian_kde
+from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
-import math, torch, os, csv
+import math, torch, os, csv, sys
+
+
+
+class General:
+	def __init__(self, project_id, data_id):
+		self.project_id = project_id
+		self.data_id = data_id
+
+		if 'results' not in os.dirlist:
+			os.mkdir('results')
+		elif f'results/{self.project_id}' not in os.dirlist('results'):
+			os.mkdir(f'results/{self.project_id}')
+
+
+	def info(self, info):
+		if 'info' not in os.listdir(f'result/{self.project_id}'):
+			os.mkdir(f'results/{self.project_id}/info')
+		
+		if f'{self.data_id}.txt' in os.listdir(f'results/{self.project_id}/info'):
+			print('ID Error: The data id already exists in the directory. Try another.')
+			sys.exit()
+		
+		with open(f'results/{self.project_id}/info/{self.data_id}.txt', 'w') as f:
+			f.write(info)
+
+		print(info)
+	
+
+	def load(self, mode, data_source):
+		if mode == 'path':
+			with open(data_source, 'r') as f:
+				self.text = f.readlines()
+		elif mode == 'url':
+			t = datetime.now()
+			print(f'Text loaded: {t.strftime("%H:%M:%S")}.')
+		else:
+			print(f'Mode Error: mode \"{mode}\" is invalid. "path" for loading a file on the device; "url" for a web scraping.')
+			sys.exit()
 
 
 
 class Embedding:
-	def __init__(self, tokenizer:str, model:str, text:list):
+	def __init__(self, tokenizer:str, model:str, process_id:str, text:list):
 		self.tokenizer = BertTokenizer.from_pretrained(tokenizer)
 		self.model = BertModel.from_pretrained(model)
 		self.text = text
 		self.dict_sws_embs = {}
 
 
-	def embed(self):
-		encoded = self.tokenizer(self.text, return_tensors='pt', truncation=True, padding=True)
-		subwords = [self.tokenizer.convert_ids_to_tokens(encoded['input_ids'][i]) for i in range(encoded['input_ids'].shape[0])]
-		output = self.model(**encoded).last_hidden_state.squeeze(0)
+	def embed(self, batch):
+		for i in range(batch,len(self.text)+1, batch):
+			batch_text = self.text[i-batch:i]
+			encoded = self.tokenizer(batch_text, return_tensors='pt', truncation=True, padding=True)
+			subwords = [self.tokenizer.convert_ids_to_tokens(encoded['input_ids'][i]) for i in range(encoded['input_ids'].shape[0])]
+			output = self.model(**encoded).last_hidden_state.squeeze(0)
 
-		for sws, embs in zip(subwords, output):
-			for sw, emb in zip(sws, embs):
-				emb = emb.unsqueeze(0)
-				if sw not in self.dict_sws_embs:
-					self.dict_sws_embs[sw] = emb
-				else:
-					self.dict_sws_embs[sw] = torch.cat((self.dict_sws_embs[sw], emb), dim=0)
+			for sws, embs in zip(subwords, output):
+				for sw, emb in zip(sws, embs):
+					emb = emb.unsqueeze(0)
+					if sw not in self.dict_sws_embs:
+						self.dict_sws_embs[sw] = emb
+					else:
+						self.dict_sws_embs[sw] = torch.cat((self.dict_sws_embs[sw], emb), dim=0)
+			percent = int(i / len(self.text) * 100)
+			process = int(percent / 2)
+			print(f'\rprocessed: |{"#"*process}{"-"*(50-process)}| {percent}%', end='')
 		
 		return self.dict_sws_embs
 	
@@ -42,9 +86,10 @@ class Embedding:
 
 
 class Density:
-	def __init__(self):
+	def __init__(self, process_id):
 		self.dict_kde = {}
 		self.list_entropy = []
+		self.process_id = process.id
 
 
 	def kde(self, dict_embeddings):
@@ -62,19 +107,22 @@ class Density:
 		self.entropy = math.mean(self.list_entropy)
 
 
-	def save(self, id):
-		if 'result.csv' not in os.listdir():
-			with open('result.csv', encoding='utf-8', mode='w') as f:
+	def save(self, data_id):
+		if 'entropy.csv' not in os.listdir(f'results/{self.process_id}'):
+			with open('entropy.csv', encoding='utf-8', mode='w') as f:
 				writer = csv.writer(f)
-				writer.writerow([id, self.entropy])
+				writer.writerow([data_id, self.entropy])
 		else:
-			with open('result.csv', encoding='utf-8', mode='a') as f:
+			with open('entropy.csv', encoding='utf-8', mode='a') as f:
 				writer = csv.writer(f)
-				writer.writerow([id, self.entropy])
+				writer.writerow([data_id, self.entropy])
+
+		if 'histograms' not in os.listdir(f'results/{self.process_id}'):
+			os.mkdir('histograms')
 
 		plt.hist(self.list_entropy, bins=50)
 		plt.axvline(self.entropy, color='red', linestyle='dashed', linewidth=2, label=f"Mean: {self.entropy:.2f}")
-		plt.title(f'{id}')
+		plt.title(f'{data_id}')
 		plt.xlabel('Entropy')
 		plt.ylabel('Frequency')
-		plt.savefig(f'{id}-histogram.png')
+		plt.savefig(f'result/{self.process_id}/histograms/{data_id}-histogram.png')
--- main.py
+++ main.py
@@ -1,46 +1,65 @@
-from classes import Embedding, Density
+#  modules
+from classes import General, Embedding, Density
 from datetime import datetime
 import argparse, time, sys
 
-
+#  main process
 if __name__ == '__main__':
+	#  parsing arguments
 	parser = argparse.ArgumentParser(description='Calculate entropy of a word in a sentence')
 	parser.add_argument('data_source', type=str, help='file path or url leading to the data source')
-	parser.add_argument('id', type=str, help='id to reference the data in output result')
+	parser.add_argument('project_id', type=str, help='id referring to the project in which output results will be in the same directory')
+	parser.add_argument('data_id', type=str, help='id referring to the data that processed in the run')
 	parser.add_argument('--mode', type=str, default='path')
 	parser.add_argument('--tokenizer', type=str, help='Tokenizer to use', default='bert-base-cased')
 	parser.add_argument('--model', type=str, help='Model to use', default='bert-base-cased')
+	parser.add_argument('--batch', type=int, help='The number of lines that are simultaneously processed into embeddings', default=100)
 	parser.add_argument('--num_samples', type=int, default=100000)
 	args = parser.parse_args()
 
+	#  starting processing
+	## outputting beginning time
 	start = time.time()
 	t = datetime.now() 
-	print(f'Analysis began: {t.strftime("%H:%M:%S")}.\nid: {args.id}\ndata source: {args.data_source}.')
+	## general info of the process
+	info = f'''Analysis began: {t.strftime("%H:%M:%S")}.
+	-----initial settings-----
+	data source: {args.data_source}
+	project id: {args.project_id}
+	data id: {args.data_id}
+	mode: {args.mode}
+	tokenizer: {args.tokenizer}
+	model: {args.model}
+	batch: {args.batch}
+	sample number: {args.num_samples}
+	--------------------------'''
 
-	if args.mode == 'path':
-		with open(args.data_source, 'r') as f:
-			text = f.readlines()
-	elif args.mode == 'url':
-		t = datetime.now()
-		print(f'Text loaded: {t.strftime("%H:%M:%S")}.')
-	else:
-		print(f'Mode Error: mode {args.mode} is invalid. "path" for loading a file on the device; "url" for a web scraping.')
-		sys.exit()
+	general = General(args.project_id, args.data_id)
+	# loading data
+	## read text in a local directory or download from the input url
+	general.info(info)
+	general.load(args.mode, args.data_source)
 
-	embedding = Embedding(args.tokenizer, args.model, text)
-	density = Density()
+	#  calling the classes
+	## for embedding and estimation of probability density
+	embedding = Embedding(args.tokenizer, args.model, args.project_id, general.text)
+	density = Density(args.project_id)
 	
-	dict_embeddings = embedding.embed()
+	#  embedding text in data
+	dict_embeddings = embedding.embed(args.batch)
 	embedding.save()
 	t = datetime.now() 
 	print(f'\n Subwords embedded: {t.strftime("%H:%M:%S")}.')
 
+	#  estimating probability density
 	density.kde(dict_embeddings)
 	density.entropy(args.num_samples)
 	density.save()
 	print(f'Entropy: {density.entropy()}.')
 
+	#  terminating the proess
 	end = time.time()
 	t = datetime.now()
 	elapsed = end - start
-	print(f'Analysis completed: {t.strftime("%H:%M:%S")}.\nProcessing time: {round(elapsed, 3)} seconds.')
+	print(f'''Analysis completed: {t.strftime("%H:%M:%S")}
+	   Processing time: {round(elapsed, 3)} seconds.''')
